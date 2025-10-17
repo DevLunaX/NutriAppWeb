752eab2c8e575646dea8b5622fd29fae700ce8e1
DevLunaX <gerardo.mau23@gmail.com>
2025-10-17 13:57:35 -0600

    Actualizado al 100%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Main } from './main/main';
+import { Citas } from './citas/citas';
+import { Recordatorio } from './recordatorio/recordatorio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component:Main},
+  {path:'citas', component:Citas},
+  {path:'recordatorio', component: Recordatorio}
+
+];
