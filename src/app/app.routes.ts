25544e1186ac3c34865001db1fca764407d624b6
DevLunaX <gerardo.mau23@gmail.com>
2025-10-28 11:16:34 -0600

    Kike mando todo a la tua

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { Main } from './main/main';
 import { Citas } from './citas/citas';
 import { Recordatorio } from './recordatorio/recordatorio';
+import { Reporte } from './reporte/reporte';
+
 
 export const routes: Routes = [
   {path:'', component:Main},
   {path:'citas', component:Citas},
-  {path:'recordatorio', component: Recordatorio}
+  {path:'recordatorio', component: Recordatorio},
+  {path:'citas', component: Citas},
+  {path: 'reporte', component:Reporte}
+
+
 
 ];
